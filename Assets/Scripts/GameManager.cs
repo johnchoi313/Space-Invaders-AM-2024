b7a7cbc10935058b0d732aee18a61660166141da
John Choi <johnchoi313@gmail.com>
2024-06-13 11:25:35 -0400

    Added sound effects and music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -31,6 +32,11 @@ public class GameManager : MonoBehaviour
     public void ActivateGameOver()
     {
         GameOverObject.SetActive(true);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("Space Invaders");
     }
 
     // Start is called before the first frame update
